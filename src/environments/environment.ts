fd62cc39f5ca804863329e5e67e8f1cc547ac2dd
angelfullstack@gmail.com <angelfullstack@gmail.com>
2022-04-19 20:41:11 +0200

    [NEW] updated environment dev

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mvTs:'1000',
+  mvPublicKey:'',
+  mvHash:''
+
 };
 
 /*
