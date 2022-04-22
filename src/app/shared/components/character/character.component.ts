d2b52a30a5a328f0534dee950312b86123a7513f
angelfullstack@gmail.com <angelfullstack@gmail.com>
2022-04-22 22:25:53 +0200

    [NEW] Basic character data on character component

--- src/app/shared/components/character/character.component.ts
+++ src/app/shared/components/character/character.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { lastValueFrom } from 'rxjs';
+import { lastValueFrom, take } from 'rxjs';
+import { Character } from '../../models/character.model';
 import { CharacterService } from '../../services/character/character.service';
 
 
@@ -11,37 +12,33 @@ import { CharacterService } from '../../services/character/character.service';
 export class CharacterComponent implements OnInit {
   @Input() characterId!: number;
   @Input() characterName!: string;
-
+  character: Character | undefined;
   constructor(private characterService: CharacterService) { }
 
  async ngOnInit(): Promise<void> {
 
-    // this.characterService.getCharacterById(123).subscribe({
+    // const character = await lastValueFrom(this.characterService.getCharacterById(this.characterId));
+    // console.log(character)
+
+    // this.characterService.getCharactersByName(this.characterName).subscribe({
     //   next: (val) => console.log(val),
     //   error: (err) => console.log(err)
     // }
     // );
 
-    // this.characterService.getCharacters().subscribe({
-    //   next: (val) => console.log(val),
-    //   error: (err) => console.log(err)
-    // }
-    // );
+    this.characterService.getCharacterById(this.characterId).subscribe({
+      next: (res)=> {
+        res?.data?.results && res.data.results.length > 0 
+        ? this.character = res.data.results[0] 
+        : null;
+        console.log(this.character)
+      },
+      error: (err) => {
+        // TODO Manage Error
+      }
+    })
 
-    const character = await lastValueFrom(this.characterService.getCharacterById(this.characterId));
-    console.log(character)
-
-    this.characterService.getCharactersByName(this.characterName).subscribe({
-      next: (val) => console.log(val),
-      error: (err) => console.log(err)
-    }
-    );
-
-
-    // this.characterService.getCharacters().subscribe({
-    // next: () => null,
-    // error: () => null}
-    // )
+    
   }
 
 }
